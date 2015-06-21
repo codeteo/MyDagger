7b6fd836689b45121ea2a4ef6ed36b6bea20d0a0
codeteo <ramones.teo@gmail.com>
2015-06-21 20:02:51 +0300

    Injects an  ArrayList

--- src/main/java/teo/dim/gomena/mydaggerexample/MainActivity.java
+++ src/main/java/teo/dim/gomena/mydaggerexample/MainActivity.java
@@ -1,15 +1,23 @@
 package teo.dim.gomena.mydaggerexample;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
     @Inject MyDataModule myDataModule;
-
-    TextView textView;
+    ArrayList<String> arrayListData;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +27,31 @@ public class MainActivity extends AppCompatActivity {
         MyApplication app = (MyApplication) getApplication();
         app.getObjectGraph().inject(this);
 
-        textView = (TextView) findViewById(R.id.my_data);
-        if (myDataModule != null) {
-            textView.setText(myDataModule.createMyDataItems());
-        } else {
-            textView.setText("Its NULL");
+        //Injects data
+        arrayListData = myDataModule.createMyDataItems();
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new MyAdapter(this, arrayListData ));
+    }
+
+
+    private class MyAdapter extends ArrayAdapter<String> {
+        public MyAdapter(Context context, ArrayList<String> data) {
+            super(context, 0, data);
         }
 
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item, parent, false);
+
+            TextView tvData = (TextView) convertView.findViewById(R.id.item_textview);
+            tvData.setText(arrayListData.get(position));
+
+            return convertView;
+        }
     }
+
 
 }
--- src/main/java/teo/dim/gomena/mydaggerexample/MyDataModule.java
+++ src/main/java/teo/dim/gomena/mydaggerexample/MyDataModule.java
@@ -1,5 +1,7 @@
 package teo.dim.gomena.mydaggerexample;
 
+import java.util.ArrayList;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -20,8 +22,12 @@ public class MyDataModule {
     }
 
 
-    public String createMyDataItems() {
-        return "MyDataString";
+    public ArrayList<String > createMyDataItems() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            arrayList.add(i, "Wow  Android " + i);
+        }
+        return arrayList;
     }
 
 }
